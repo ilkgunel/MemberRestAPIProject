2221964de6064ecdb75068ff24d00762a82f0b93
Ilkay Günel <ilkgunel93@gmail.com>
2018-02-24 11:14:09 -0800

    Logging added to member get service.

--- src/main/java/com/ilkaygunel/service/MemberGetService.java
+++ src/main/java/com/ilkaygunel/service/MemberGetService.java
@@ -1,11 +1,14 @@
 package com.ilkaygunel.service;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ilkaygunel.entities.Member;
+import com.ilkaygunel.logging.LoggingUtil;
 import com.ilkaygunel.repository.MemberRepository;
 
 @Service
@@ -14,14 +17,36 @@ public class MemberGetService {
 	private MemberRepository memberRepository;
 
 	public List<Member> getAllMemberList() {
-		return (List<Member>) memberRepository.findAll();
+		Logger LOGGER = new LoggingUtil().getLoggerForMemberGetting(this.getClass());
+		try {
+			LOGGER.log(Level.INFO, "getAllMemberList() method is running.");
+			return (List<Member>) memberRepository.findAll();
+		} catch (Exception ex) {
+			LOGGER.log(Level.SEVERE, "An error occured while getting all members. Error is:" + ex.getMessage());
+		}
+		return null;
 	}
 
 	public Member getMemberViaId(long id) {
-		return memberRepository.findOne(id);
+		Logger LOGGER = new LoggingUtil().getLoggerForMemberGetting(this.getClass());
+		try {
+			LOGGER.log(Level.INFO, "getMemberViaId(...) method is running.");
+			return memberRepository.findOne(id);
+		} catch (Exception ex) {
+			LOGGER.log(Level.SEVERE, "An error occured while getting member via id. Error is:" + ex.getMessage());
+		}
+		return null;
 	}
 
 	public Member getMemberViaFirstName(String firstName) {
-		return memberRepository.findByFirstName(firstName);
+		Logger LOGGER = new LoggingUtil().getLoggerForMemberGetting(this.getClass());
+		try {
+			LOGGER.log(Level.INFO, "getMemberViaFirstName(...) method is running.");
+			return memberRepository.findByFirstName(firstName);
+		} catch (Exception ex) {
+			LOGGER.log(Level.SEVERE,
+					"An error occured while getting member via first name. Error is:" + ex.getMessage());
+		}
+		return null;
 	}
 }
