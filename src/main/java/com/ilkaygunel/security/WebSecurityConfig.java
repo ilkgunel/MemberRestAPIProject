7f835f0192dbe72d3678bb0b44dd7b46159979be
ilkgunel <ilkgunel93@gmail.com>
2018-04-21 22:45:29 +0300

    Spring Data used for Spring Security

--- src/main/java/com/ilkaygunel/security/WebSecurityConfig.java
+++ src/main/java/com/ilkaygunel/security/WebSecurityConfig.java
@@ -1,28 +1,26 @@
 package com.ilkaygunel.security;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-@PropertySource("classpath:sqlQueries.properties")
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
-	private DataSource dataSource;
+	private UserDetailsService userDetailsService;
 
-	@Autowired
-	private Environment env;
+	@Bean
+	private BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -33,16 +31,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.authorizeRequests().anyRequest().authenticated().and().httpBasic();
 	}
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.jdbcAuthentication().dataSource(dataSource).usersByUsernameQuery(env.getProperty("membersByEmailQuery"))
-				.authoritiesByUsernameQuery(env.getProperty("authoritiesByEmailQuery"))
-				.passwordEncoder(new BCryptPasswordEncoder());
-	}
-
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		web.debug(true);
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
+		authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 	}
 
 }
