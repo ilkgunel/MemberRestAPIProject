c3082e499eda22366097c914ff515b293e7af2d3
ASSECOSEETR\ilkay.gunel <ilkay.gunel@asseco-see.com.tr>
2018-05-01 15:43:54 +0300

    Internalization Development

--- src/main/java/com/ilkaygunel/application/ApplicationConfig.java
+++ src/main/java/com/ilkaygunel/application/ApplicationConfig.java
@@ -1,16 +1,24 @@
 package com.ilkaygunel.application;
 
-import org.springframework.context.MessageSource;
+import java.util.Locale;
+
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ApplicationConfig {
 
-	public MessageSource messageSource() {
-		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-		messageSource.setBasenames("messageTexts");
-		messageSource.setDefaultEncoding("UTF-8");
-		return messageSource;
+	public static ResourceBundleMessageSource messageSourceInstance = null;
+
+	public String getValueOfProperty(String propertyName, String localeValue) {
+
+		if (messageSourceInstance == null) {
+			messageSourceInstance = new ResourceBundleMessageSource();
+			messageSourceInstance.setBasenames("messageTexts");
+			messageSourceInstance.setDefaultEncoding("UTF-8");
+		}
+
+		return messageSourceInstance.getMessage(propertyName, null, new Locale(localeValue));
+
 	}
 }
--- src/main/java/com/ilkaygunel/service/MemberSaveService.java
+++ src/main/java/com/ilkaygunel/service/MemberSaveService.java
@@ -2,62 +2,60 @@ package com.ilkaygunel.service;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Locale;
 import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import com.ilkaygunel.entities.MemberRoles;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.ilkaygunel.constants.ConstantFields;
 import com.ilkaygunel.entities.Member;
+import com.ilkaygunel.entities.MemberRoles;
 import com.ilkaygunel.exception.CustomException;
 import com.ilkaygunel.pojo.MemberOperationPojo;
 
 @PropertySource(ignoreResourceNotFound = true, value = "classpath:errorMeanings.properties")
 @PropertySource(ignoreResourceNotFound = true, value = "classpath:messageTexts.properties")
 @Service
-public class MemberSaveService extends BaseService{
+public class MemberSaveService extends BaseService {
 
 	public MemberOperationPojo addOneUserMember(Member member) {
-		MemberOperationPojo memberOperationPojo = addOneMember(member,ConstantFields.ROLE_USER.getConstantField());
+		MemberOperationPojo memberOperationPojo = addOneMember(member, ConstantFields.ROLE_USER.getConstantField());
 		return memberOperationPojo;
 
 	}
 
 	public MemberOperationPojo addOneAdminMember(Member member) {
-		return addOneMember(member,ConstantFields.ROLE_ADMIN.getConstantField());
+		return addOneMember(member, ConstantFields.ROLE_ADMIN.getConstantField());
 	}
 
 	public MemberOperationPojo addBulkUserMember(List<Member> memberList) {
-		return addBulkMember(memberList,ConstantFields.ROLE_USER.getConstantField());
+		return addBulkMember(memberList, ConstantFields.ROLE_USER.getConstantField());
 	}
 
 	public MemberOperationPojo addBulkAdminMember(List<Member> memberList) {
-		return addBulkMember(memberList,ConstantFields.ROLE_ADMIN.getConstantField());
+		return addBulkMember(memberList, ConstantFields.ROLE_ADMIN.getConstantField());
 	}
 
-	public MemberOperationPojo addOneMember(Member member,String role) {
+	public MemberOperationPojo addOneMember(Member member, String role) {
 		MemberOperationPojo memberOperationPojo = new MemberOperationPojo();
 		Logger LOGGER = loggingUtil.getLoggerForMemberSaving(this.getClass());
-		try {			
-			LOGGER.log(Level.INFO, applicationConfig.messageSource().getMessage(role + "_memberAddingMethod", null, new Locale("tr")));
+		try {
+			LOGGER.log(Level.INFO, applicationConfig.getValueOfProperty(role + "_memberAddingMethod", "tr"));
 			memberUtil.checkEmailAddress(member);
 			member.setPassword(getHashedPassword(member.getPassword()));
 			member.setEnabled(false);
-			addMemberRolesObject(role,member);
-            addActivationToken(member);
+			addMemberRolesObject(role, member);
+			addActivationToken(member);
 			memberRepository.save(member);
-			mailUtil.sendActivationMail(member.getEmail(),member.getActivationToken());
-			memberOperationPojo
-					.setResult(environment.getProperty(role + "_memberAddingSuccessfull") + member);
+			mailUtil.sendActivationMail(member.getEmail(), member.getActivationToken());
+			memberOperationPojo.setResult(environment.getProperty(role + "_memberAddingSuccessfull") + member);
 			LOGGER.log(Level.INFO, environment.getProperty(role + "_memberAddingSuccessfull") + member);
 		} catch (CustomException e) {
-			LOGGER.log(Level.SEVERE, environment.getProperty(role + "_memberAddingFaled") + e.getErrorCode()
-					+ " " + e.getErrorMessage());
+			LOGGER.log(Level.SEVERE, environment.getProperty(role + "_memberAddingFaled") + e.getErrorCode() + " "
+					+ e.getErrorMessage());
 			memberOperationPojo.setErrorCode(e.getErrorCode());
 			memberOperationPojo.setResult(e.getErrorMessage());
 		} catch (Exception e) {
@@ -67,7 +65,7 @@ public class MemberSaveService extends BaseService{
 		return memberOperationPojo;
 	}
 
-	public MemberOperationPojo addBulkMember(List<Member> memberList,String role) {
+	public MemberOperationPojo addBulkMember(List<Member> memberList, String role) {
 		MemberOperationPojo memberOperationPojo = new MemberOperationPojo();
 		Logger LOGGER = loggingUtil.getLoggerForMemberSaving(this.getClass());
 		try {
@@ -76,23 +74,20 @@ public class MemberSaveService extends BaseService{
 				memberUtil.checkEmailAddress(member);
 				member.setPassword(getHashedPassword(member.getPassword()));
 				member.setEnabled(true);
-				addMemberRolesObject(role,member);
+				addMemberRolesObject(role, member);
 				addActivationToken(member);
 				memberRepository.save(member);
-                mailUtil.sendActivationMail(member.getEmail(),member.getActivationToken());
+				mailUtil.sendActivationMail(member.getEmail(), member.getActivationToken());
 			}
-			memberOperationPojo.setResult(
-					environment.getProperty(role + "_bulkMemberAddingSuccessfull") + memberList);
-			LOGGER.log(Level.INFO,
-					environment.getProperty(role + "_bulkMemberAddingSuccessfull") + memberList);
+			memberOperationPojo.setResult(environment.getProperty(role + "_bulkMemberAddingSuccessfull") + memberList);
+			LOGGER.log(Level.INFO, environment.getProperty(role + "_bulkMemberAddingSuccessfull") + memberList);
 		} catch (CustomException e) {
-			LOGGER.log(Level.SEVERE, environment.getProperty(role + "_bulkMemberAddingFaled")
-					+ e.getErrorCode() + " " + e.getErrorMessage());
+			LOGGER.log(Level.SEVERE, environment.getProperty(role + "_bulkMemberAddingFaled") + e.getErrorCode() + " "
+					+ e.getErrorMessage());
 			memberOperationPojo.setErrorCode(e.getErrorCode());
 			memberOperationPojo.setResult(e.getErrorMessage());
 		} catch (Exception e) {
-			LOGGER.log(Level.SEVERE,
-					environment.getProperty(role + "_bulkMemberAddingFaled") + e.getMessage());
+			LOGGER.log(Level.SEVERE, environment.getProperty(role + "_bulkMemberAddingFaled") + e.getMessage());
 			memberOperationPojo.setResult(e.getMessage());
 		}
 		return memberOperationPojo;
@@ -102,19 +97,20 @@ public class MemberSaveService extends BaseService{
 		return new BCryptPasswordEncoder().encode(rawPassword);
 	}
 
-	private void addMemberRolesObject(String role,Member member){
+	private void addMemberRolesObject(String role, Member member) {
 		MemberRoles rolesOfMember = new MemberRoles();
 		rolesOfMember.setRole(role);
 		rolesOfMember.setEmail(member.getEmail());
 		member.setRoleOfMember(rolesOfMember);
 	}
 
-	private void addActivationToken(Member member){
-        String activationToken = UUID.randomUUID().toString();
-        member.setActivationToken(activationToken);
+	private void addActivationToken(Member member) {
+		String activationToken = UUID.randomUUID().toString();
+		member.setActivationToken(activationToken);
 
-        LocalDateTime activationTokenExpDate = LocalDateTime.now().plusDays(1);
-        //LocalDateTime activationTokenExpDate = LocalDateTime.now();//Use for expire date test!
-        member.setActivationTokenExpDate(activationTokenExpDate);
-    }
+		LocalDateTime activationTokenExpDate = LocalDateTime.now().plusDays(1);
+		// LocalDateTime activationTokenExpDate = LocalDateTime.now();//Use for expire
+		// date test!
+		member.setActivationTokenExpDate(activationTokenExpDate);
+	}
 }
