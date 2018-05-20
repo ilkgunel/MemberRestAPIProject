ae46aa8aa7234e193fe260b8209a176b36f469f8
İlkay Günel <ilkay.gunel@kod5.org>
2018-05-20 22:35:26 +0300

    Bug Fix and code refactoring

--- src/main/java/com/ilkaygunel/service/MemberDeleteService.java
+++ src/main/java/com/ilkaygunel/service/MemberDeleteService.java
@@ -13,70 +13,79 @@ import com.ilkaygunel.entities.Member;
 import com.ilkaygunel.exception.CustomException;
 import com.ilkaygunel.pojo.MemberOperationPojo;
 import com.ilkaygunel.wrapper.MemberIdWrapp;
+import org.springframework.util.ObjectUtils;
 
 @Service
-public class MemberDeleteService extends BaseService{
+public class MemberDeleteService extends BaseService {
 
-	public MemberOperationPojo deleteOneUserMember(long memberId) {
-		MemberOperationPojo memberOperationPojo = deleteOneMember(memberId, ConstantFields.ROLE_USER.getConstantField());
-		return memberOperationPojo;
-	}
+    public MemberOperationPojo deleteOneUserMember(long memberId) {
+        MemberOperationPojo memberOperationPojo = deleteOneMember(memberId, ConstantFields.ROLE_USER.getConstantField());
+        return memberOperationPojo;
+    }
 
-	public MemberOperationPojo deleteOneAdminMember(long memberId) {
-		MemberOperationPojo memberOperationPojo =  deleteOneMember(memberId, ConstantFields.ROLE_ADMIN.getConstantField());
-		return memberOperationPojo;
-	}
+    public MemberOperationPojo deleteOneAdminMember(long memberId) {
+        MemberOperationPojo memberOperationPojo = deleteOneMember(memberId, ConstantFields.ROLE_ADMIN.getConstantField());
+        return memberOperationPojo;
+    }
 
-	public MemberOperationPojo deleteBulkUserMember(List<MemberIdWrapp> memberIdList) {
-		MemberOperationPojo memberOperationPojo = new MemberOperationPojo();
-		List<Member> deletedMemberList = new ArrayList<>();
-		for (MemberIdWrapp memberIdWrapp : memberIdList) {
-			MemberOperationPojo temporaryMemberOperationPojo = deleteOneMember(memberIdWrapp.getId(),
-					ConstantFields.ROLE_USER.getConstantField());
-			deletedMemberList.add(temporaryMemberOperationPojo.getMemberList().get(0));
-		}
-		memberOperationPojo.setResult(environment.getProperty(ConstantFields.ROLE_USER + "_bulkMemberDeletingSuccessfull"));
-		memberOperationPojo.setMemberList(deletedMemberList);
-		return memberOperationPojo;
-	}
+    public MemberOperationPojo deleteBulkUserMember(List<MemberIdWrapp> memberIdList) {
+        MemberOperationPojo deleteMemberOpertaionPojo = memberUtil.checkMemberExistenceOnMemberList(memberIdList, ConstantFields.ROLE_USER.getConstantField());
+        MemberOperationPojo memberOperationPojo = new MemberOperationPojo();
+        if (ObjectUtils.isEmpty(deleteMemberOpertaionPojo.getErrorCode())) {
 
-	public MemberOperationPojo deleteBulkAdminMember(List<MemberIdWrapp> memberIdList) {
-		MemberOperationPojo memberOperationPojo = new MemberOperationPojo();
-		List<Member> deletedMemberList = new ArrayList<>();
-		for (MemberIdWrapp memberIdWrapp : memberIdList) {
-			MemberOperationPojo temporaryMemberOperationPojo = deleteOneMember(memberIdWrapp.getId(),
-					ConstantFields.ROLE_ADMIN.getConstantField());
-			deletedMemberList.add(temporaryMemberOperationPojo.getMemberList().get(0));
-		}
-		memberOperationPojo.setResult(environment.getProperty(ConstantFields.ROLE_ADMIN + "_bulkMemberDeletingSuccessfull"));
-		memberOperationPojo.setMemberList(deletedMemberList);
-		return memberOperationPojo;
-	}
+            List<Member> deletedMemberList = new ArrayList<>();
+            for (MemberIdWrapp memberIdWrapp : memberIdList) {
+                MemberOperationPojo temporaryMemberOperationPojo = deleteOneMember(memberIdWrapp.getId(),
+                        ConstantFields.ROLE_USER.getConstantField());
+                deletedMemberList.add(temporaryMemberOperationPojo.getMemberList().get(0));
+            }
+            memberOperationPojo.setResult(resourceBundleMessageManager.getValueOfProperty(ConstantFields.ROLE_USER + "_bulkMemberDeletingSuccessfull", deletedMemberList.get(0).getMemberLanguageCode()));
+            memberOperationPojo.setMemberList(deletedMemberList);
 
-	public MemberOperationPojo deleteOneMember(long memberId, String roleOfMember) {
-		List<Member> deletedMemberList = new ArrayList<>();
+        } else {
+            memberOperationPojo.setErrorCode(deleteMemberOpertaionPojo.getErrorCode());
+            memberOperationPojo.setResult(deleteMemberOpertaionPojo.getResult());
+        }
+        return memberOperationPojo;
+    }
 
-		MemberOperationPojo memberOperationPojo = new MemberOperationPojo();
-		Logger LOGGER = loggingUtil.getLoggerForMemberDeleting(this.getClass());
-		try {
-			LOGGER.log(Level.INFO, environment.getProperty(roleOfMember + "_memberDeletingMethod"));
-			Member memberForDelete = memberUtil.checkMember(memberId, roleOfMember);
-			memberRepository.delete(memberId);
-			deletedMemberList.add(memberForDelete);
-			memberOperationPojo.setMemberList(deletedMemberList);
-			memberOperationPojo
-					.setResult(environment.getProperty(roleOfMember + "_memberDeletingSuccessfull") + memberForDelete);
-			LOGGER.log(Level.INFO,
-					environment.getProperty(roleOfMember + "_memberDeletingSuccessfull") + memberForDelete);
-		} catch (CustomException e) {
-			LOGGER.log(Level.SEVERE, environment.getProperty(roleOfMember + "_memberDeletingFailed") + e.getErrorCode()
-					+ " " + e.getErrorMessage());
-			memberOperationPojo.setErrorCode(e.getErrorCode());
-			memberOperationPojo.setResult(e.getErrorMessage());
-		} catch (Exception e) {
-			LOGGER.log(Level.SEVERE, environment.getProperty(roleOfMember + "_memberDeletingFailed") + e.getMessage());
-			memberOperationPojo.setResult(e.getMessage());
-		}
-		return memberOperationPojo;
-	}
+    public MemberOperationPojo deleteBulkAdminMember(List<MemberIdWrapp> memberIdList) {
+        MemberOperationPojo memberOperationPojo = new MemberOperationPojo();
+        List<Member> deletedMemberList = new ArrayList<>();
+        for (MemberIdWrapp memberIdWrapp : memberIdList) {
+            MemberOperationPojo temporaryMemberOperationPojo = deleteOneMember(memberIdWrapp.getId(),
+                    ConstantFields.ROLE_ADMIN.getConstantField());
+            deletedMemberList.add(temporaryMemberOperationPojo.getMemberList().get(0));
+        }
+        memberOperationPojo.setResult(environment.getProperty(ConstantFields.ROLE_ADMIN + "_bulkMemberDeletingSuccessfull"));
+        memberOperationPojo.setMemberList(deletedMemberList);
+        return memberOperationPojo;
+    }
+
+    public MemberOperationPojo deleteOneMember(long memberId, String roleOfMember) {
+        List<Member> deletedMemberList = new ArrayList<>();
+
+        MemberOperationPojo memberOperationPojo = new MemberOperationPojo();
+        Logger LOGGER = loggingUtil.getLoggerForMemberDeleting(this.getClass());
+        try {
+            LOGGER.log(Level.INFO, environment.getProperty(roleOfMember + "_memberDeletingMethod"));
+            Member memberForDelete = memberUtil.checkMember(memberId, roleOfMember);
+            memberRepository.delete(memberId);
+            deletedMemberList.add(memberForDelete);
+            memberOperationPojo.setMemberList(deletedMemberList);
+            memberOperationPojo
+                    .setResult(environment.getProperty(roleOfMember + "_memberDeletingSuccessfull") + memberForDelete);
+            LOGGER.log(Level.INFO,
+                    environment.getProperty(roleOfMember + "_memberDeletingSuccessfull") + memberForDelete);
+        } catch (CustomException e) {
+            LOGGER.log(Level.SEVERE, environment.getProperty(roleOfMember + "_memberDeletingFailed") + e.getErrorCode()
+                    + " " + e.getErrorMessage());
+            memberOperationPojo.setErrorCode(e.getErrorCode());
+            memberOperationPojo.setResult(e.getErrorMessage());
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, environment.getProperty(roleOfMember + "_memberDeletingFailed") + e.getMessage());
+            memberOperationPojo.setResult(e.getMessage());
+        }
+        return memberOperationPojo;
+    }
 }
--- src/main/java/com/ilkaygunel/util/MemberUtil.java
+++ src/main/java/com/ilkaygunel/util/MemberUtil.java
@@ -4,6 +4,7 @@ import com.ilkaygunel.application.ResourceBundleMessageManager;
 import com.ilkaygunel.entities.MemberRoles;
 import com.ilkaygunel.pojo.MemberOperationPojo;
 import com.ilkaygunel.service.MemberRoleSaveService;
+import com.ilkaygunel.wrapper.MemberIdWrapp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
@@ -44,16 +45,16 @@ public class MemberUtil {
         MemberRoles memberRoles = memberRoleSaveService.getMemberRoleWithEmail(member.getEmail());
         if (member == null) {
             if (ConstantFields.ROLE_USER.equals(roleForCheck)) {
-                throw new CustomException(ErrorCodes.ERROR_01.getErrorCode(), resourceBundleMessageManager.getValueOfProperty(ErrorCodes.ERROR_01.getErrorCode(),"en"));
+                throw new CustomException(ErrorCodes.ERROR_01.getErrorCode(), resourceBundleMessageManager.getValueOfProperty(ErrorCodes.ERROR_01.getErrorCode(), "en"));
             } else {
-                throw new CustomException(ErrorCodes.ERROR_01.getErrorCode(), resourceBundleMessageManager.getValueOfProperty(ErrorCodes.ERROR_02.getErrorCode(),"en"));
+                throw new CustomException(ErrorCodes.ERROR_01.getErrorCode(), resourceBundleMessageManager.getValueOfProperty(ErrorCodes.ERROR_02.getErrorCode(), "en"));
             }
         } else if (ConstantFields.ROLE_USER.equals(roleForCheck)
                 && !ConstantFields.ROLE_USER.equals(memberRoles.getRole())) {
-            throw new CustomException(ErrorCodes.ERROR_03.getErrorCode(), resourceBundleMessageManager.getValueOfProperty(ErrorCodes.ERROR_03.getErrorCode(),"en"));
+            throw new CustomException(ErrorCodes.ERROR_03.getErrorCode(), resourceBundleMessageManager.getValueOfProperty(ErrorCodes.ERROR_03.getErrorCode(), "en"));
         } else if (ConstantFields.ROLE_ADMIN.equals(roleForCheck)
                 && !ConstantFields.ROLE_ADMIN.equals(memberRoles.getRole())) {
-            throw new CustomException(ErrorCodes.ERROR_04.getErrorCode(), resourceBundleMessageManager.getValueOfProperty(ErrorCodes.ERROR_04.getErrorCode(),"en"));
+            throw new CustomException(ErrorCodes.ERROR_04.getErrorCode(), resourceBundleMessageManager.getValueOfProperty(ErrorCodes.ERROR_04.getErrorCode(), "en"));
         }
         return member;
     }
@@ -68,13 +69,13 @@ public class MemberUtil {
         MemberOperationPojo memberOperationPojo = new MemberOperationPojo();
         try {
             if (ObjectUtils.isEmpty(member.getEmail())) {
-                throw new CustomException(ErrorCodes.ERROR_05.getErrorCode(), resourceBundleMessageManager.getValueOfProperty(ErrorCodes.ERROR_05.getErrorCode(),"en"));
+                throw new CustomException(ErrorCodes.ERROR_05.getErrorCode(), resourceBundleMessageManager.getValueOfProperty(ErrorCodes.ERROR_05.getErrorCode(), "en"));
             } else if (memberRepository.findByEmail(member.getEmail()) != null) {
-                throw new CustomException(ErrorCodes.ERROR_06.getErrorCode(),resourceBundleMessageManager.getValueOfProperty(ErrorCodes.ERROR_06.getErrorCode(),"en")+ " " + member.getEmail());
+                throw new CustomException(ErrorCodes.ERROR_06.getErrorCode(), resourceBundleMessageManager.getValueOfProperty(ErrorCodes.ERROR_06.getErrorCode(), "en") + " " + member.getEmail());
             } else if (!isValidEmailAddress(member.getEmail())) {
-                throw new CustomException(ErrorCodes.ERROR_07.getErrorCode(), resourceBundleMessageManager.getValueOfProperty(ErrorCodes.ERROR_07.getErrorCode(),"en")+ " " + member.getEmail());
-            }else if(ObjectUtils.isEmpty(member.getMemberLanguageCode())){
-                throw new CustomException(ErrorCodes.ERROR_11.getErrorCode(), resourceBundleMessageManager.getValueOfProperty(ErrorCodes.ERROR_11.getErrorCode(),"en"));
+                throw new CustomException(ErrorCodes.ERROR_07.getErrorCode(), resourceBundleMessageManager.getValueOfProperty(ErrorCodes.ERROR_07.getErrorCode(), "en") + " " + member.getEmail());
+            } else if (ObjectUtils.isEmpty(member.getMemberLanguageCode())) {
+                throw new CustomException(ErrorCodes.ERROR_11.getErrorCode(), resourceBundleMessageManager.getValueOfProperty(ErrorCodes.ERROR_11.getErrorCode(), "en"));
             }
         } catch (CustomException customException) {
             memberOperationPojo.setErrorCode(customException.getErrorCode());
@@ -83,11 +84,27 @@ public class MemberUtil {
         return memberOperationPojo;
     }
 
+    public MemberOperationPojo checkMemberExistenceOnMemberList(List<MemberIdWrapp> memberIdList, String roleForCheck) {
+        MemberOperationPojo memberOperationPojo = new MemberOperationPojo();
+        try {
+            for (MemberIdWrapp memberIdWrapp : memberIdList) {
+                checkMember(memberIdWrapp.getId(), roleForCheck);
+            }
+        } catch (CustomException customException) {
+            memberOperationPojo.setErrorCode(customException.getErrorCode());
+            memberOperationPojo.setResult(customException.getErrorMessage());
+        } catch (Exception exception) {
+            memberOperationPojo.setErrorCode(ErrorCodes.ERROR_10.getErrorCode());
+            memberOperationPojo.setResult(exception.getMessage());
+        }
+        return memberOperationPojo;
+    }
+
     public void checkEmailAddressOnMemberList(List<Member> memberList, Logger LOGGER) throws CustomException {
         MemberOperationPojo memberOperationPojo = null;
         for (Member member : memberList) {
             memberOperationPojo = checkEmailAddressAndLanguage(member, LOGGER);
-            if (!ObjectUtils.isEmpty(memberOperationPojo.getErrorCode())){
+            if (!ObjectUtils.isEmpty(memberOperationPojo.getErrorCode())) {
                 throw new CustomException(memberOperationPojo.getErrorCode(), memberOperationPojo.getResult());
             }
         }
